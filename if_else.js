8e78a20b9719b75f9aad73467d215fee33d29033
DhruvNaithani <dhruvnaithani8@gmail.com>
2026-03-08 10:26:40 +0530

    if-else statement

--- if_else.js
+++ if_else.js
@@ -1,5 +1,7 @@
 document.write("if-else staement <br>");
 var year=2025;
+document.write("a leap year is a calendar year containing 366 days <br>");
+document.write("input year is ",year,"<br>");
 if(year%4==0 && year%100!=0){
     document.write("leap year");
 }
