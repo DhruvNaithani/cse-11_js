8a377449121c4c661d31cb1d624640b7487e992e
DhruvNaithani <dhruvnaithani8@gmail.com>
2026-03-10 19:21:46 +0530

    objects in js

---
+++ object.js
@@ -0,0 +1,12 @@
+const details={
+    name:"dhruv",
+    last_name:"naithani",
+    branch:"cse",
+    serial_num:45,
+    university_roll_num:2503201000457,
+    subjects:{
+        names:["maths","web dev","dsa","mechanis","electronics"],
+        credits:[4,3,4,4,3]
+    }
+}
+console.log(details.subjects.names);
