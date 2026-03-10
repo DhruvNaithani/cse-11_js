b687833636db6a77ee623520a4759a55b5810e16
DhruvNaithani <dhruvnaithani8@gmail.com>
2026-03-10 19:19:06 +0530

    arrays in js

---
+++ array.js
@@ -0,0 +1,17 @@
+let collection=[
+    {},
+    [],
+    12,
+    true,
+    "hello world",
+    function(){
+        return "i am a function";
+    },
+    5678,
+    null,
+    undefined,
+    {name:"rahul",age:25},
+    false
+]
+console.log(collection);
+document.write(collection);
